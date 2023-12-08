168150ee3b17a53ba5ba1f455c5961b3e3f08089
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-12-08 16:07:54 -0300

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -168,7 +168,7 @@ const Banner = () => {
                 <FaLinkedin />
               </a>
               <a
-                href="https://drive.google.com/file/d/15s8XWAx4aBz29q-Nwd2doNXl6rzBnqou/view?usp=sharing"
+                href="https://drive.google.com/file/d/1VVZCflgVRvBqf3S1wq1-9HGW_8CoGnt0/view?usp=drive_link"
                 target="blank"
               >
                 <FaFileDownload />
